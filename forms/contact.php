d4736f0f098289cd8c7087f0595ee21988466f0e
Patrick <patvhdez@gmail.com>
2023-06-22 23:12:58 -0400

    #

--- forms/contact.php
+++ forms/contact.php
@@ -8,12 +8,19 @@
 
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'patvhdez@gmail.com';
-
-  if( file_exists('../assets/vendor/php-email-form/php-email-form.php')) {
+  
+  $php_email_form = '../assets/vendor/php-email-form/php-email-form.php';
+if(file_exists($php_email_form)) {
+    include($php_email_form);
+} else {
+    die('Coming Soon!');
+}
+  /*if( file_exists('../assets/vendor/php-email-form/php-email-form.php')) {
     include('../assets/vendor/php-email-form/php-email-form.php');
 } else {
     die( 'Coming Soon!');
 }
+*/
 
   /*if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
