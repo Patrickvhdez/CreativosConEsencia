d2f4716ad8fb38908e50924ee52861ab4d383f21
Patrick <patvhdez@gmail.com>
2023-06-25 21:48:18 -0400

    #new contact form

--- forms/contact.php
+++ forms/contact.php
@@ -23,20 +23,21 @@
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
+  $contact->message = $_POST['message'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'patvhdez@gmail.com',
+    'password' => 'Verizon2022@',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['subject'], 'Subject', 10);
-
+  $contact->add_message( $_POST['message'], 'Message');
   echo $contact->send();
 ?>
